4d691cdfdccc75eccb00ae70ec447f32cdd2a9d4
un4ckn0wl3z <slickerth@gmail.com>
2019-12-07 23:11:09 +0700

    clustering

--- FakeCode/index.js
+++ FakeCode/index.js
@@ -1,8 +1,30 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster')
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
-});
+// master?
+if (cluster.isMaster) {
+    // Cause index.js to be excuted *agin* but in slave mode.
+    cluster.fork();
+} else {
 
-app.listen(3000);
+    // slave code.
+
+    const express = require('express');
+    const app = express();
+
+    function doWork(duration) {
+        const start = Date.now();
+        while (Date.now() - start < duration) { }
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast!!');
+    });
+
+    app.listen(3000);
+}
+
